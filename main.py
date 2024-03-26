dcce13b019d6ff53bad9c6313ca49385d4043e22
JONATHAN THOMAS URK21CS2036 <91773936+jonathanthomas19@users.noreply.github.com>
2024-03-26 15:09:29 +0530

    Routing

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
-
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
-import os
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
 # Serve static files. This makes the frontend and its assets accessible.
 app.mount("/static", StaticFiles(directory="frontend"), name="static")
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/")
 async def main():
-    # Path to the index.html file
-    index_html_path = os.path.join("frontend", "index.html")
-    
-    # Read and return the contents of the index.html file
-    with open(index_html_path, 'r') as file:
-        html_content = file.read()
-    
-    return HTMLResponse(content=html_content)
+    # Redirect to the index.html page served under /static
+    return RedirectResponse(url="/static/index.html")
